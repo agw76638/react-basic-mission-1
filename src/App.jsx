8f77a6e0919bfe4cb8537d3dc01ca4b44e717573
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-22 16:49:50 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Card from './components/Card';
+
+function App() {
+  return (
+    <>
+      <Card name='무선 키보드' price={39000} brand='EZTECH'></Card>
+    </>
+  );
+}
+
+export default App;
