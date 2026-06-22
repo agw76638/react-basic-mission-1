b6e7a7b30bd354ba84690fc3b0fc54c613308845
Gunnwook Ahn <agw76638@protonmail.com>
2026-06-22 16:52:15 +0900

    fix: 변수 할당

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import Card from './components/Card';
+const name = '무선 키보드';
+const price = 39000;
+const brand = 'EZTECH';
 
 function App() {
   return (
     <>
-      <Card name='무선 키보드' price={39000} brand='EZTECH'></Card>
+      <Card name={name} price={price} brand={brand}></Card>
     </>
   );
 }
